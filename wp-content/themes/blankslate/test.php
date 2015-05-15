49733892e588ea8b99fe9bf8829bebc139f52d09
Joseph Fitzgerald <joefitzgerald@Josephs-iMac.local>
2015-05-15 11:07:47 -0400

    Why page template

---
+++ wp-content/themes/blankslate/test.php
@@ -0,0 +1,71 @@
+<div id="site_bleed" class="grey_back">
+    <div id="site_contain" style="overflow: auto;">
+        <div id="site_content_contain">
+            <h1>Why Healthstaff</h1>
+            <div id="why_content">
+                <h3>The Right Choice For Your Future</h3>
+                <p>HealthStaff Training Institute (HSTi) is a professional, private vocational institution that trains individuals into becoming efficient, capable allied health care or business professionals. HSTi is dedicated to provide quality, entry-level vocational training in relatively short period of time. Our industry-focused courses and curricula are designed to properly train and educate students interested in the several allied health or business programs we offer.</p>
+                <p>Why not listen in to what our students and successful alumni have to say? Read some of their testimonials here.</p>
+            </div>
+            <div id="why_content">
+                <h3>Our Philosophy</h3>
+                <p>HealthStaff Training Institute is dedicated to provide quality, entry-level vocational training in a relatively short period of time at an affordable cost. The industry-focused courses and curriculum are geared to close the employment skills gap between our graduates and the allied health or business job they are trained for. As health care becomes of critical concern, the properly trained health-care professionals becomes a priority. HealthStaff Training Institute endeavors to train and place competent persons. In addition, HealthStaff Training Insitute assumes responsibility for each of our students by using practical teaching techniques, equipment commonly used in the field, industry-focused curriculum, and experienced, qualified faculty to develop the skills and job responsibility of our students. </p>
+            </div>
+            <div id="why_content">
+                <h3>Our History</h3>
+                <p>HealthStaff Training Institute (HSTi) began in September 1986 when the education and training division of Pharmacy Enterprises, Inc. formed an education division. When the anticipated expansion of HSTi exceeded the scope of Pharmacy Enterprises, the institute became a privately owned entity, carrying on the name of HealthStaff Training Institute. Since 1986, we have grown to offer a broad range of courses and a well-respected faculty. Our institute continues to evolve to accommodate the needs of the healthcare marketplace and the interest of our students. Every day we carry forward the mission to train and educate motivated and focused individuals into becoming efficient, capable allied health care or business professionals.</p>
+            </div>
+        </div>
+        <div id="site_sidebar">
+            <div id="our_approval">
+                <h2>Approval</h2>
+                <p>HealthStaff Training Institute has been granted institutional “Approval To Operate” by the Bureau of Private Postsecondary Education, which means that this institution and its operations comply with the state standards established under the law of occupational instruction by private postsecondary educational institutions.</p>
+                <p>Anyone who wants to contact the bureau can do so by going to their website at www.bppe.ca.gov or by mail at 2535 Capitol Oaks Drive, Suite 400, Sacramento, CA 95833 or P.O. Box 980818 West Sacramento, CA 95798-0818 or by phone 888-370-7589 (toll-free) or 916-431-6959 or by fax 916-263-1897. </p>
+            </div>
+            <div id="our_ceo">
+                <h2>From the CEO</h2>
+                <p>While this site contains a lot of information about our programs and resources, the words and photos only give you a glimpse into what is so special about our school.</p>
+            </div>
+            <div id="our_mission">
+                <h2>Our Mission</h2>
+                <p>The mission of HealthStaff Training Institute is to provide quality entry-level and retraining in vocational educational programs that are sound in concept and design and geared to serve those seeking solid foundations in the allied health and general business industries.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="site_contain" style="text-align: left;">
+    <div id="top_content">
+        <div id="our_mission">
+            <h2>Our Mission</h2>
+            <p>The mission of HealthStaff Training Institute is to provide quality entry-level and retraining in vocational educational programs that are sound in concept and design and geared to serve those seeking solid foundations in the allied health and general business industries.</p>
+        </div>
+        <div id="our_ceo">
+            <h2>From the CEO</h2>
+            <p>While this site contains a lot of information about our programs and resources, the words and photos only give you a glimpse into what is so special about our school.</p>
+        </div>
+        <div id="our_approval">
+            <h2>Approval</h2>
+            <p>HealthStaff Training Institute has been granted institutional “Approval To Operate” by the Bureau of Private Postsecondary Education, which means that this institution and its operations comply with the state standards established under the law of occupational instruction by private postsecondary educational institutions.</p>
+            <p>Anyone who wants to contact the bureau can do so by going to their website at www.bppe.ca.gov or by mail at 2535 Capitol Oaks Drive, Suite 400, Sacramento, CA 95833 or P.O. Box 980818 West Sacramento, CA 95798-0818 or by phone 888-370-7589 (toll-free) or 916-431-6959 or by fax 916-263-1897. </p>
+        </div>
+    </div>
+</div>
+<div id="why_section">
+    <div id="site_contain" style="text-align: left;">
+        <h1>Why Healthstaff</h1>
+        <div id="why_content">
+            <h3>The Right Choice For Your Future</h3>
+            <p>HealthStaff Training Institute (HSTi) is a professional, private vocational institution that trains individuals into becoming efficient, capable allied health care or business professionals. HSTi is dedicated to provide quality, entry-level vocational training in relatively short period of time. Our industry-focused courses and curricula are designed to properly train and educate students interested in the several allied health or business programs we offer.</p>
+            <p>Why not listen in to what our students and successful alumni have to say? Read some of their testimonials here.</p>
+        </div>
+        <div id="why_content">
+            <h3>Our History</h3>
+            <p>HealthStaff Training Institute (HSTi) began in September 1986 when the education and training division of Pharmacy Enterprises, Inc. formed an education division. When the anticipated expansion of HSTi exceeded the scope of Pharmacy Enterprises, the institute became a privately owned entity, carrying on the name of HealthStaff Training Institute. Since 1986, we have grown to offer a broad range of courses and a well-respected faculty. Our institute continues to evolve to accommodate the needs of the healthcare marketplace and the interest of our students. Every day we carry forward the mission to train and educate motivated and focused individuals into becoming efficient, capable allied health care or business professionals.</p>
+        </div>
+        <div id="why_content">
+            <h3>Our Philosophy</h3>
+            <p>HealthStaff Training Institute is dedicated to provide quality, entry-level vocational training in a relatively short period of time at an affordable cost. The industry-focused courses and curriculum are geared to close the employment skills gap between our graduates and the allied health or business job they are trained for. As health care becomes of critical concern, the properly trained health-care professionals becomes a priority. HealthStaff Training Institute endeavors to train and place competent persons. In addition, HealthStaff Training Insitute assumes responsibility for each of our students by using practical teaching techniques, equipment commonly used in the field, industry-focused curriculum, and experienced, qualified faculty to develop the skills and job responsibility of our students. </p>
+        </div>
+    </div>
+</div>
